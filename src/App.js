5987f1f5b26f2a95d6819aa05e819d5f374b9303
kscub@Satya17 <ksatya1705@gmail.com>
2023-08-13 18:23:49 +0530

    useState exercise completed !

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import {useState} from 'react';
 function App() {
 
 
-     const[inputvalue,setinputvalue]=useState("");                                                                 //[variable,function]--useState
-const handleinputchange=(event)=>{setinputvalue(event.target.value);};
+     const[count,setCount]=useState(0);                                                                //[variable,function]--useState
+
 
 
   return (
     <div className="App">
-    <input type="text" onChange={handleinputchange}/>
-    {inputvalue}
+      <button onClick={()=>{setCount(count+1)}}>Increase</button>
+      <button onClick={()=>{setCount(count-1)}}>Decrease</button>
+      <button onClick={()=>{setCount(count*0)}}>Set to Zero</button>
+{count}
     </div>
   );
 }
