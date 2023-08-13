090e93044b0630ed95dcfebafb303d6c7ca1fe72
kscub@Satya17 <ksatya1705@gmail.com>
2023-08-13 17:40:31 +0530

    Learnt useState hook and react events

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import "./App.css";
-
+import {useState} from 'react';
 function App() {
-  const planets = [
-    { name: "Mars", isGasPlanet: false },
-    { name: "Earth", isGasPlanet: false },
-    { name: "Jupiter", isGasPlanet: true },
-    { name: "Venus", isGasPlanet: false },
-    { name: "Neptune", isGasPlanet: true },
-    { name: "Uranus", isGasPlanet: true },
-  ];
 
+const[age, setAge]=useState(0);
+ //[variable,function]--useState
+const increaseAge=()=>{
+setAge(age+1)
+};
   return (
     <div className="App">
-      {planets.map(
-        (planet, key) => planet.isGasPlanet && <h1> {planet.name} </h1>
-      )}
+    {age} <button onClick={increaseAge}>Increase Age</button>
     </div>
   );
 }
