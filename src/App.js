0d293416783cc6e077b5ed8b4cac24e1b1456214
kscub@Satya17 <ksatya1705@gmail.com>
2023-08-14 19:25:53 +0530

    Created a todo list using crud operations off react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import "./App.css";
-import {useState} from 'react';
+import { useState } from "react";
+import { Task } from "./task";
+
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
 
-     const[count,setCount]=useState(0);                                                                //[variable,function]--useState
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+    setTodoList(task.taskName !== "" ? [...todoList, task] : todoList);
+  };
 
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
 
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
 
   return (
     <div className="App">
-      <button onClick={()=>{setCount(count+1)}}>Increase</button>
-      <button onClick={()=>{setCount(count-1)}}>Decrease</button>
-      <button onClick={()=>{setCount(count*0)}}>Set to Zero</button>
-{count}
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}> Add Task</button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
