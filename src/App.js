2f46f36f739f0084f7bf516c0d15f2c517c38675
kscub@Satya17 <ksatya1705@gmail.com>
2023-08-24 01:00:34 +0530

    Learnt about react router dom

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 
 import "./App.css";
 import Axios from "axios";
-import{useEffect,useState} from "react"
-
-
+import{useEffect,useState} from "react";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {Homepage} from "./Pages/Homepage";
+import {Menu} from "./Pages/Menu";
+import {Navbar} from "./Navbar";
 function App() {
-    const[name,setname]=useState("");
-    const[predictedage,setpredictedage]=useState(null);
-    const fetchdata=()=>{
-        Axios.get(`https://api.agify.io/?name=${name}`).then((res)=>{
-            setpredictedage(res.data);
-            });
-    };
-    useEffect(()=>{
-       fetchdata();
-    },[]);
+   
 
 
     return(
 <div className="App">
-    <input placeholder="" onChange={(event)=>{setname(event.target.value);}}/>
-    <button onClick={fetchdata}>Predict Age</button>
-    <h1>  Name: {predictedage?.name}</h1>
-    <h1>  Predicted Age: {predictedage?.age}</h1>
     
+    <Router>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="*" element={<h1>Page not found</h1>} />
+        </Routes>
+    </Router>
   </div>
     );
    
---
+++ src/Pages/Menu.js
@@ -0,0 +1,3 @@
+export const Menu=()=>{
+    return <h1>this is menu page</h1>;
+};
