cdc3bbddddca79a01e6e913e7fa17b66b1a7b1c6
kscub@Satya17 <ksatya1705@gmail.com>
2023-08-26 23:24:21 +0530

    react-hook-form, yup, yup-resolver learnt

--- src/Pages/Profile.js
+++
@@ -1,7 +0,0 @@
-import {useContext} from "react";
-import {AppContext} from '../App';
-import {ChangeProfile} from "../components/ChangeProfile"
-export const Profile=(props)=>{
-    const {username}=useContext(AppContext);
-    return <div>Profile, user is: {username} <ChangeProfile /></div>;
-};
