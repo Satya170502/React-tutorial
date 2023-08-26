3184f0cec8d54a71d3b32baca26fac0100ad19e4
kscub@Satya17 <ksatya1705@gmail.com>
2023-08-27 02:16:10 +0530

    Exercise of custom hooks completed

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@
 import "./App.css";
 import {useState} from "react"
 import { useToggle } from "./useToggle";
+import { useCount } from "./useCount";
 
 function App() {
-   const[isVisible,toggle]=useToggle();
-   const[isVisible2,toggle2]=useToggle();
+   const[count,increase,decrease,reset]=useCount();
  
 
     return(<div className="App">
- <button onClick={toggle}>{isVisible?"Hide":"Show"}</button>
- {isVisible && <h1>HIdden text</h1>}
- <button onClick={toggle2}>{isVisible2?"Hide":"Show"}</button>
- {isVisible2 && <h1>HIdden text</h1>}
+      {count}
+    <button onClick={increase}>+</button>
+    <button onClick={decrease}>-</button>
+    <button onClick={reset}>Reset</button>
 
     </div>);
    
